c55e73bc838512c13bcaf571e78ff47a14b2c3cf
DieynabouD <diallo.159@osu.edu>
2024-09-02 18:25:47 -0400

    Removed submodule and added directory

---
+++ BuiltMathFunctions.rb
@@ -0,0 +1,11 @@
+def sin(n)
+  Math.sin(n)
+end
+
+def cos(n)
+  Math.cos(n)
+end
+
+def tan(n)
+  Math.tan(n)
+end
---
+++ MathFunctions.rb
@@ -0,0 +1,30 @@
+def exponent(a, b)
+  a ** b
+end
+
+
+def absolute(a)
+  if a < 0
+    -a
+  else
+    a
+  end
+end
+
+
+def generate_square_numbers(start, stop, file_path)
+  File.open(file_path, 'w') do |file|
+    (start..stop).each do |num|
+      file.puts(num ** 2)
+    end
+  end
+end
+
+
+def generate_even_numbers(start, stop, file_path)
+  File.open(file_path, 'w') do |file|
+    (start..stop).each do |num|
+      file.puts(num) if num.even?
+    end
+  end
+end
