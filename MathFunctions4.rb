0123d0cab1f02449fe46068d69a5cfad372ce8cd
Noelle <marcy.14@osu.edu>
2024-09-15 18:26:31 -0400

    All complete except for minor changes needed for fibonacci function

---
+++ MathFunctions4.rb
@@ -0,0 +1,74 @@
+
+# dataset = [1,3,10,4,5,8]
+# for data in dataset do
+#     puts data
+# end
+
+
+#function to calculate the mean (average) of a dataset
+def mean(dataset)
+i=0
+a=0
+sum=0
+puts "sum: #{sum}, a: #{a}, i: #{i}"
+while (i!=dataset.length())
+   a=dataset[i]
+   puts "a: #{a}"
+   sum=sum+a 
+   puts "sum: #{sum}"
+   i=i+1
+end
+result=sum/2
+puts "#{result}"
+end
+
+
+#function to return the largest value from a set of numbers
+def maximum()
+i=0
+a=0
+largest=0
+while (i!=dataset.length())
+   a=dataset[i]
+   puts "a: #{a}"
+   if(a>largest)
+     largest=a 
+   end
+   puts "largest (in loop): #{largest}"
+   i=i+1
+end
+puts "largest: #{largest}"
+end
+
+ # #function to generate fibonacci numbers up to a given limit and save them to a file
+ def fibonacci()
+puts "Please give limit: "
+limit=gets.chomp().to_i
+i=1
+a=1
+b=0
+c=0
+if(limit!=0)
+  print "1"
+  while(i!=limit)
+      c=a+b #takes first two numbers and adds them
+      print " #{c}"
+      b=a #b goes up to a
+      a=c #a goes up to c
+      i=i+1
+      
+  end
+else
+  puts "0"
+end
+ end
+
+# #function to convert fahrenheit to celsius
+def fahrenheit_to_celsius()
+    puts "Please enter your fahrenheit value: "
+    f=gets.chomp.to_i
+    a=(f-32)
+    fraction=Rational(5,9)
+    result=a*(fraction)
+    puts "Celsius result: #{format("%.2f",result)}"
+ end
