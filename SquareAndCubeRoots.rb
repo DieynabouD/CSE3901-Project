163152d6bbe05fecf43702943b08d267904ba755
Yutong Ye <ye.1039@osu.edu>
2024-09-16 21:41:25 -0400

    update

--- SquareAndCubeRoots.rb
+++ SquareAndCubeRoots.rb
@@ -4,8 +4,9 @@ def squareRoot(n)
 end
 
 # Method to get cube roots.
-def cube_root(number)
-  cube_root_value = number ** (1.0 / 3.0)
+def cubeRoot(n)
+  cube_root_value = n ** (1.0 / 3.0)
   return cube_root_value
 end
+
 
